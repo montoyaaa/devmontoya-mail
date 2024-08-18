1e6702a67cce3d38f26640cf2b3d804433317730
Ricardo Montoya <ricardomontoya2002@hotmail.com>
2024-08-18 15:03:13 -0300

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { EmailParams, MailerSend } from "mailersend";
+import cors from "cors";
+const app = express();
+const port = 3000;
+
+const mailerSend = new MailerSend({
+  apiKey:
+    "mlsn.4cbaa1dd3d19e68a2b6369d55bf5e50d0dde28ee071f097347b76da8dafb04a8",
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/email", async (req, res) => {
+  const emailParams = new EmailParams()
+    .setFrom(req.body.from)
+    .setTo(req.body.to)
+    .setSubject(req.body.subject)
+    .setText(req.body.text);
+  console.log(req.body);
+
+  await mailerSend.email
+    .send(emailParams)
+    .then(() => res.send("Ok"))
+    .catch((d) => res.send("Oops, não foi possível enviar" + d));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
