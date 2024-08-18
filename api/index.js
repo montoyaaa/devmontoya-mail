c095e6a66f13eda58ec2875b335a4d262d1b4206
Ricardo Montoya <ricardomontoya2002@hotmail.com>
2024-08-18 15:49:21 -0300

    try build to vercel

--- api/index.js
+++ api/index.js
@@ -1,35 +1,8 @@
-import express from "express";
-import { EmailParams, MailerSend } from "mailersend";
-import cors from "cors";
-import { env } from "process";
+const express = require("express");
+const app = express();
 
-export const app = express();
-const port = env.PORT ?? 3000;
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-const mailerSend = new MailerSend({
-  apiKey:
-    "mlsn.4cbaa1dd3d19e68a2b6369d55bf5e50d0dde28ee071f097347b76da8dafb04a8",
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => res.send("On the line!"));
-
-app.post("/email", async (req, res) => {
-  const emailParams = new EmailParams()
-    .setFrom(req.body.from)
-    .setTo(req.body.to)
-    .setSubject(req.body.subject)
-    .setText(req.body.text);
-  console.log(req.body);
-
-  await mailerSend.email
-    .send(emailParams)
-    .then(() => res.send("Ok"))
-    .catch((d) => res.send("Oops, não foi possível enviar" + d));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
