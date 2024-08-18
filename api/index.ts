c3b2b43b0546c305e3081822d42b168ae1d6fb80
Ricardo Montoya <ricardomontoya2002@hotmail.com>
2024-08-18 16:27:06 -0300

    try build to vercel

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
-export const app = express();
+const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
+
+export default app;
